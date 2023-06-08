730473ea5fa1c13f7333c5a13ad7f47b5b06ea18
stivenVA <stivenv.ariza@gmail.com>
2023-06-08 16:02:17 -0500

    Creacion del proyecto

---
+++ src/main/java/com/codingideas/cashkeeper/CashKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.codingideas.cashkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingideas/cashkeeper/CashKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingideas.cashkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
